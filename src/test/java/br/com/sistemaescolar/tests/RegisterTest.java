e04bd068cadfb75bf03d214c5ad56dd9ada7651c
William Colombo Breda <williamcolombobreda@gmail.com>
2024-05-20 22:17:32 -0300

    add homolog branch

--- src/test/java/br/com/sistemaescolar/tests/RegisterTest.java
+++ src/test/java/br/com/sistemaescolar/tests/RegisterTest.java
@@ -1,11 +1,13 @@
 package br.com.sistemaescolar.tests;
 
 import br.com.sistemaescolar.data.CsvEntryRegister;
+import com.epam.reportportal.service.ReportPortal;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.Calendar;
 import java.util.concurrent.ExecutionException;
 
 import static br.com.sistemaescolar.helpers.BuildHelpers.buildPojo;
@@ -21,14 +23,16 @@ public class RegisterTest {
     @CsvFileSource(resources = "/auth-register/auth-register.csv", numLinesToSkip = 1)
     @DisplayName("Register Tests")
     void register(ArgumentsAccessor accessor) throws ExecutionException, InterruptedException {
-        var csvEntry = csv.mapToCsvEntry(accessor);
-        var pojo = buildPojo.apply("auth-register", csvEntry.getCenario().equalsIgnoreCase("ok")
-                ?retornaUserDinamico.apply(csvEntry.getLogin())
-                :csvEntry.getLogin(), csvEntry.getPassword(), csvEntry.getRole(), null);
+//        var csvEntry = csv.mapToCsvEntry(accessor);
+//        var pojo = buildPojo.apply("auth-register", csvEntry.getCenario().equalsIgnoreCase("ok")
+//                ?retornaUserDinamico.apply(csvEntry.getLogin())
+//                :csvEntry.getLogin(), csvEntry.getPassword(), csvEntry.getRole(), null);
+//
+//        var request = asyncRequest.apply(pojo);
+//
+//        Assertions.assertEquals(request.get().statusCode(), Integer.valueOf(csvEntry.getStatus()));
 
-        var request = asyncRequest.apply(pojo);
-
-        Assertions.assertEquals(request.get().statusCode(), Integer.valueOf(csvEntry.getStatus()));
+        ReportPortal.emitLog("teste", "info", Calendar.getInstance().getTime());
 
     }
 }
