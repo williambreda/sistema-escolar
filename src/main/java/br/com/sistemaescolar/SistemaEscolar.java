8bc6a6182e2db42037fad74bcde79761af3f7a1f
William Colombo Breda <williamcolombobreda@gmail.com>
2024-05-20 23:38:00 -0300

    ajuste descricao

--- src/main/java/br/com/sistemaescolar/SistemaEscolar.java
+++ src/main/java/br/com/sistemaescolar/SistemaEscolar.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info=@Info(title="Sistema Escolar V1.0 - Teste Deploy"))
+@OpenAPIDefinition(info=@Info(title="Sistema Escolar V1.0 - Teste Deploy TCC"))
 @SpringBootApplication
 @SecurityScheme(
 		name = "Bearer Authentication",
