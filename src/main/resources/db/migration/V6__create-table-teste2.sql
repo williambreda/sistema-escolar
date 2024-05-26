6b07068457ae470b0c80c9eb75fb08d0b3bcb9e4
William Colombo Breda <williamcolombobreda@gmail.com>
2024-05-26 14:13:33 -0300

    criacao tabela teste2

---
+++ src/main/resources/db/migration/V6__create-table-teste2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE teste2 (
+     id text COLLATE pg_catalog."default" NOT NULL,
+     login text COLLATE pg_catalog."default" NOT NULL,
+     test text COLLATE pg_catalog."default" NOT NULL
+);
