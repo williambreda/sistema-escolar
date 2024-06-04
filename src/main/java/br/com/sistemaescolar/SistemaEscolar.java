139be9478146ee89f0f6ecd8cc51eaafeba49454
William Colombo Breda <williamcolombobreda@gmail.com>
2024-06-03 21:45:06 -0300

    ajuste nome ambiente

--- src/main/java/br/com/sistemaescolar/SistemaEscolar.java
+++ src/main/java/br/com/sistemaescolar/SistemaEscolar.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@OpenAPIDefinition(info=@Info(title="Sistema Escolar V1.0 - " + "${ENV}" + "Testes Deploy"))
+@OpenAPIDefinition(info=@Info(title="Sistema Escolar V1.0 - " + "${ENV}"))
 @SpringBootApplication
 @SecurityScheme(
 		name = "Bearer Authentication",
